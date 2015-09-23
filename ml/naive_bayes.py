d6329ae1fdedafe002380b3e11fa4fb67693ae58
fydot <ifidot@gmail.com>
2015-09-23 18:23:37 +0800

    add bayes classification

--- ml/naive_bayes.py
+++ ml/naive_bayes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf8
 from collections import Counter
+import random
+import math
 
 
 def load_data_set():
@@ -26,7 +28,8 @@ def words2vec(vocabList, inputSet):
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
-        else: print "the word: %s is not in my Vocabulary!" % word
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
     return returnVec
 
 
@@ -43,22 +46,47 @@ def train_nb(data, labels):
         for idx in labels_idx[label]:
             point = data[idx]
             for i in range(len(point)):
-                if point[i] == 1:
-                    label_feature_cnt[label][i] += 1
+                label_feature_cnt[label][i] += point[i]
 
     label_feature_p = {}
     for label in label_feature_cnt:
         label_feature_p[label] = []
         for i in range(len(data[0])):
-            count = sum([label_feature_cnt[label][i] for i in label_feature_cnt[label]])
-            label_feature_p[label].append(label_feature_cnt[label][i] * 1.0 / count)
+            count = sum([label_feature_cnt[label][i] for i in label_feature_cnt[label]]) + 2
+            label_feature_p[label].append(math.log((label_feature_cnt[label][i] + 1) * 1.0 / count, 2))
 
     return label_feature_p, {label: len(labels_idx[label]) * 1.0 / len(labels) for label in labels_idx}
 
 
+def classify(features_p, label_p, vector):
+    def mult(v1, v2):
+        return [x * y for x, y in zip(v1, v2)]
+
+    return [(label, sum(mult(vector, features_p[label])) + math.log(label_p[label], 2)) for label in features_p]
+
+
+def gen_data(m, n):
+    data = []
+    for i in range(m):
+        data.append([])
+        for j in range(n):
+            data[i].append(int(random.randint(1, 10) >= 5))
+    return data
+
+def gen_labels(n):
+    return [map(chr, range(65, 91))[random.randint(1, 100000) % 10] for i in range(n)]
+
 if __name__ == "__main__":
-    posts, labels = load_data_set()
-    features = get_featurs(posts)
-    data = [words2vec(features, post) for post in posts]
+    # posts, labels = load_data_set()
+    # features = get_featurs(posts)
+    # data = [words2vec(features, post) for post in posts]
+    # p_feature, p_label = train_nb(data, labels)
+    data = gen_data(100, 10)
+    labels = gen_labels(100)
     p_feature, p_label = train_nb(data, labels)
-    print features, p_feature, p_label
+
+    # for label, vector in zip(labels, data):
+    #     print label, vector
+
+    vector = gen_data(1, 10)[0]
+    print vector, sorted(classify(p_feature, p_label, vector), key=lambda x: x[1])[-1][0]
